06a9f38827a8bc9ff9c24405e58ff1c106996d89
Chiara Riente <chiarariente@users.noreply.github.com>
2017-02-26 10:16:03 +0100

    clock

--- sketch.js
+++ sketch.js
@@ -1,48 +1,93 @@
 var hourColor = "#ff6666";
 var minuteColor = "#009999";
+var yText;
 
 function drawSecond() {
+  if (width<=height){
   strokeWeight(2.5);
-  line(15,0,-130,0);
-  ellipse(0,0,10,10);
+  line(15,0,-width/4,0);
+  ellipse(0,0,8,8);
+  } else { 
+    strokeWeight(4);
+    line(20,0,-width/7,0);
+    ellipse(0,0,10,10);
+    }
 }
 
 function drawMinute() {
-  strokeWeight(2.5);
   stroke(minuteColor);
   noFill();
-  beginShape();
-    vertex(105,-105);
-    vertex(105,105);
-    vertex(-105,105);
-    vertex(-105,-105);
-    vertex(-7,-105);
-    vertex(-7,-20);
-    vertex(7,-20);
-    vertex(7,-105);
-    vertex(105,-105);
-  endShape();
+  if(width<=height){
+  strokeWeight(2.5);
+    beginShape();
+      vertex(width/4,-width/4);
+      vertex(width/4,width/4);
+      vertex(-width/4,width/4);
+      vertex(-width/4,-width/4);
+      vertex(-5,-width/4);
+      vertex(-5,-20);
+      vertex(5,-20);
+      vertex(5,-width/4);
+      vertex(width/4,-width/4);
+    endShape();
+  } else {
+    strokeWeight(4);
+    beginShape();
+      vertex(width/8,-width/8);
+      vertex(width/8,width/8);
+      vertex(-width/8,width/8);
+      vertex(-width/8,-width/8);
+      vertex(-8,-width/8);
+      vertex(-8,-30);
+      vertex(8,-30);
+      vertex(8,-width/8);
+      vertex(width/8,-width/8);
+    endShape();
+    }
 }
 
 function drawHour() {
-  strokeWeight(2.5);
   stroke(hourColor);
   noFill();
-  beginShape();
-    vertex(98,-98);
-    vertex(98,98);
-    vertex(-98,98);
-    vertex(-98,-98);
-    vertex(-7,-98);
-    vertex(-7,-45);
-    vertex(7,-45);
-    vertex(7,-98);
-    vertex(98,-98);
-  endShape();
+  if(width<=height){
+  strokeWeight(2.5);
+    beginShape();
+      vertex(width/5,-width/5);
+      vertex(width/5,width/5);
+      vertex(-width/5,width/5);
+      vertex(-width/5,-width/5);
+      vertex(-5,-width/5);
+      vertex(-5,-35);
+      vertex(5,-35);
+      vertex(5,-width/5);
+      vertex(width/5,-width/5);
+    endShape();
+  } else {
+    strokeWeight(4);
+    beginShape();
+      vertex(width/9,-width/9);
+      vertex(width/9,width/9);
+      vertex(-width/9,width/9);
+      vertex(-width/9,-width/9);
+      vertex(-8,-width/9);
+      vertex(-8,-75);
+      vertex(8,-75);
+      vertex(8,-width/9);
+      vertex(width/9,-width/9);
+    endShape();
+  }
+}
+
+function myText() {
+  if (width<=height) {
+    yText = height/3.5;
+  } else {
+    yText = height/2.7;
+    }
 }
 
 function setup() {
-  createCanvas(800,600);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 }
 
@@ -53,7 +98,7 @@ var s=second();
 
   translate(width/2,height/2-30);
   
-  if (h>=20 || h<=8 ) {
+  if (h>=20 && h<=8) {
   background(70);
   stroke(255);
   fill(255);
@@ -71,38 +116,42 @@ var s=second();
   push();
   if (h<=12) {
     rotate((h*30)+(m/2));
-    } else { rotate(((h-12)*30)+(m/2)); }
+    } else { rotate((h-12*30)+m/2); }
   drawHour();
   pop();
   
   push();
-  rotate((s*6)+90);
+  rotate(s*6+90);
   drawSecond();
   pop();
  
+  myText();
   textFont("Courier", 18);
   
   push();
   fill(hourColor);
   noStroke();
+  myText();
   if (h>=0 && h<=9) {
-  text("0"+h+":", -45, 210);
-  } else { text(h+":", -45, 210); } 
+  text("0"+h+":", -45, yText);
+  } else { text(h+":", -45, yText); } 
   pop();
   
   push();
   fill(minuteColor);
   noStroke();
+  myText();
   if(m>=0 && m<=9) {
-  text("0"+m+":", -10, 210);
-  } else { text(m+":", -10, 210); }
+  text("0"+m+":", -10, yText);
+  } else { text(m+":", -10, yText); }
   pop();
   
   push();
   noStroke();
+  myText();
   if(s>=0 && s<=9) {
-  text("0"+s, 25, 210);
-  } else {text(s, 25, 210);}
+  text("0"+s, 25, yText);
+  } else {text(s, 25, yText);}
   pop();
   
 }
